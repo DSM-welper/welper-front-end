be5fab3ab71a71c5b90b9c4250f9bf1f224ed8ad
Srim <deamoon0801@naver.com>
2021-03-28 00:09:00 +0900

    [ADD] <FirstScreen>

--- src/components/FirstView/FirstScreen.js
+++ src/components/FirstView/FirstScreen.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Image from "next/image";
 import "./FirstView.scss";
-import { first_view_mock, a } from "../../assets/img";
+import { welper_logo, first_view2_mock } from "../../assets/img";
 
 const FirstScreen = () => {
   return (
     <div className="first-container">
-      <Image src={a} alt="background" width={300} height={300}></Image>
-      <Image src={first_view_mock} width={400} height={300}></Image>
+      <div className="inner-container">
+        <div>
+          <Image src={welper_logo} alt="welper_logo" width={140} height={70}></Image>
+          <h1>
+            WELCOME TO <br />WELPER
+          </h1>
+          <Image src={first_view2_mock} alt="first_view_mock" width={370} height={200}></Image>
+          <p>웰퍼를 통해 더 나은 세상을 함께해보세요.</p>
+          <button>시작하기</button>
+        </div>
+      </div>
     </div>
   );
 };
