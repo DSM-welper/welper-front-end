f5c57e24aa2186e293e971eb8f602401b2f7a2e4
Srim <deamoon0801@naver.com>
2021-03-28 15:58:59 +0900

    [MODIFY] <FirstScreen> button

--- src/components/FirstView/FirstScreen.js
+++ src/components/FirstView/FirstScreen.js
@@ -10,11 +10,12 @@ const FirstScreen = () => {
         <div>
           <Image src={welper_logo} alt="welper_logo" width={140} height={70}></Image>
           <h1>
-            WELCOME TO <br />WELPER
+            WELCOME TO <br />
+            WELPER
           </h1>
           <Image src={first_view2_mock} alt="first_view_mock" width={370} height={200}></Image>
           <p>웰퍼를 통해 더 나은 세상을 함께해보세요.</p>
-          <button>시작하기</button>
+          <button className="main-button">시작하기</button>
         </div>
       </div>
     </div>
