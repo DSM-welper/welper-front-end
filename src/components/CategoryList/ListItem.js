e4fc4ad4964ff3cb2492ff6e037f356cfed6961e
김해린 <rin3583@naver.com>
2021-03-31 23:38:29 +0900

    [ADD] category list UI

---
+++ src/components/CategoryList/ListItem.js
@@ -0,0 +1,66 @@
+import "./Category.scss";
+import { category_datail } from "../../assets/img";
+
+const ListItem = () => {
+  return (
+    <div className="list-container">
+      <div className="list-box">
+        <p>시간제 보육</p>
+        <div className="content-box">
+          <span>
+            가정 양육 시에도 필요한 때에 필요한 만큼 이용할 수 있는 보육
+            서비스를 제공하여 자녀 양육에 대한 부담을 경감하고 부모의 교육
+            서비스 선택권을 보장합니다
+          </span>
+        </div>
+        <img src={category_datail} />
+      </div>
+      <div className="list-box">
+        <p>시간제 보육</p>
+        <div className="content-box">
+          <span>
+            가정 양육 시에도 필요한 때에 필요한 만큼 이용할 수 있는 보육
+            서비스를 제공하여 자녀 양육에 대한 부담을 경감하고 부모의 교육
+            서비스 선택권을 보장합니다
+          </span>
+        </div>
+        <img src={category_datail} />
+      </div>
+      <div className="list-box">
+        <p>시간제 보육</p>
+        <div className="content-box">
+          <span>
+            가정 양육 시에도 필요한 때에 필요한 만큼 이용할 수 있는 보육
+            서비스를 제공하여 자녀 양육에 대한 부담을 경감하고 부모의 교육
+            서비스 선택권을 보장합니다
+          </span>
+        </div>
+        <img src={category_datail} />
+      </div>
+      <div className="list-box">
+        <p>시간제 보육</p>
+        <div className="content-box">
+          <span>
+            가정 양육 시에도 필요한 때에 필요한 만큼 이용할 수 있는 보육
+            서비스를 제공하여 자녀 양육에 대한 부담을 경감하고 부모의 교육
+            서비스 선택권을 보장합니다
+          </span>
+        </div>
+        <img src={category_datail} />
+      </div>
+      <div className="list-box">
+        <p>시간제 보육</p>
+        <div className="content-box">
+          <span>
+            가정 양육 시에도 필요한 때에 필요한 만큼 이용할 수 있는 보육
+            서비스를 제공하여 자녀 양육에 대한 부담을 경감하고 부모의 교육
+            서비스 선택권을 보장합니다
+          </span>
+        </div>
+        <img src={category_datail} />
+      </div>
+    </div>
+  );
+};
+
+export default ListItem;
