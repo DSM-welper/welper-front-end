b100fca5d218de15e6e912b49ed10730701cda9c
김해린 <rin3583@naver.com>
2021-03-31 23:38:09 +0900

    [ADD] DetailView Ui

---
+++ src/components/Detail/DetailView.js
@@ -0,0 +1,14 @@
+import PageTemplate from "../common/PageTemplate/PageTemplate";
+import WelpareDetail from "./WelpareDetail";
+
+const DetailView = () => {
+  return (
+    <div className="background-public">
+      <PageTemplate>
+        <WelpareDetail></WelpareDetail>
+      </PageTemplate>
+    </div>
+  );
+};
+
+export default DetailView;
