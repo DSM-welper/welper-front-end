1cd4f7e9d65f75cfb204b1e0c0a4bc3cb8eb3aad
Srim <deamoon0801@naver.com>
2021-04-02 19:24:45 +0900

    [ADD] <FirstView> get recommend list

--- src/components/FirstView/RecommendItem.js
+++ src/components/FirstView/RecommendItem.js
@@ -2,15 +2,13 @@ import React from "react";
 import Image from "next/image";
 import { handshake } from "../../assets/img";
 
-const RecommendItem = () => {
+const RecommendItem = ({ title, description, category }) => {
   return (
     <div className="recommend-item-box">
       <Image src={handshake} alt="handshake_icon" width={50} height={30}></Image>
-      <h2>어쩌구 복지정책</h2>
-      <p className="description-text">
-        이 정책은 한달에 어쩌구 두달동안 어쩌구를 하신분들에게 제공하는 어쩌구 이 정책은 한달에 어쩌구 두달동안 어쩌구를 하신분들에게 제공하는 어쩌구…
-      </p>
-      <p>청소년기</p>
+      <h2>{title && title.length > 13 ? `${title.substr(0, 13)}...` : title}</h2>
+      <p className="description-text">{description && description.length > 66 ? `${description.substr(0, 66)}...` : description}</p>
+      <p>{category}</p>
     </div>
   );
 };
