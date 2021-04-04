9f539009176807dea023cf275cf3abc206547005
김해린 <rin3583@naver.com>
2021-04-04 20:20:04 +0900

    [ADD] category api

--- src/lib/api/category.js
+++ src/lib/api/category.js
@@ -1,1 +1,45 @@
+import getClientAccessToken from "./client";
+const qs = require("qs");
+export async function AllCategory(numOfPage) {
+  const { data } = await getClientAccessToken.get("/category", numOfPage);
+  return {
+    data,
+  };
+}
 
+export async function CategoryDetail(id) {
+  const { data } = await getClientAccessToken.get(
+    "http://13.125.199.215:8080" + `/category/detail/${id}`
+  );
+  return {
+    data,
+  };
+}
+
+export async function CategorySearch(content, numOfPage) {
+  const { data } = await getClientAccessToken.get("/category/search", {
+    content,
+    numOfPage,
+  });
+  return {
+    data,
+  };
+}
+
+export async function CategoryTag(numOfPage, categoryName) {
+  const { data } = await getClientAccessToken.get(
+    "http://13.125.199.215:8080" + "/category/tag",
+    {
+      params: {
+        numOfPage: numOfPage,
+        categoryName: categoryName,
+      },
+      paramsSerializer: (params) => {
+        return qs.stringify(params, { arrayFormat: "repeat" });
+      },
+    }
+  );
+  return {
+    data,
+  };
+}
