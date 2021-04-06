730ae761d96890b60cbb8e2aee17a9242256064e
김해린 <rin3583@naver.com>
2021-04-06 10:53:09 +0900

    [EDIT] detail view data

--- src/components/Detail/WelpareDetail.js
+++ src/components/Detail/WelpareDetail.js
@@ -1,52 +1,89 @@
 import "./Detail.scss";
 
-const WelpareDetail = () => {
+const WelpareDetail = ({ data }) => {
   return (
     <div className="detail-box">
       <p>
         <span style={{ color: "#802457" }}>무엇</span>인가요?
       </p>
-      <p className="title">시간제 보육 복지정책</p>
+      <p className="title">{data.servNm}</p>
       <p className="content">
-        가정 양육 시에도 필요한 때에 필요한 만큼 이용할 수 있는 보육 서비스를
-        제공하여 자녀 양육에 대한 부담을 경감하고 부모의 보육 서비스 선택권을
-        보장합니다.가정 양육 시에도 필요한 때에 필요한 만큼 이용할 수 있는 보육
-        서비스를 제공하여 자녀 양육에 대한 부담을 경감하고 부모의 보육 서비스
-        선택권을 보장합니다.
+        {data.servDgst}
+        <br></br>
+        <br></br>
+        {data.alwServCn}
       </p>
       <p>
         <span style={{ color: "#802457" }}>누가</span> 할 수 있나요?
       </p>
-      <p className="title">시간제 보육 복지정책</p>
-      <p className="content">
-        가정 양육 시에도 필요한 때에 필요한 만큼 이용할 수 있는 보육 서비스를
-        제공하여 자녀 양육에 대한 부담을 경감하고 부모의 보육 서비스 선택권을
-        보장합니다.가정 양육 시에도 필요한 때에 필요한 만큼 이용할 수 있는 보육
-        서비스를 제공하여 자녀 양육에 대한 부담을 경감하고 부모의 보육 서비스
-        선택권을 보장합니다.
-      </p>
+      <p className="title">{data.tgtrDtlCn}</p>
+      <p className="content">{data.slctCritCn}</p>
       <p>
         <span style={{ color: "#802457" }}>어떻게</span> 하나요?
       </p>
-      <p className="title">시간제 보육 복지정책</p>
-      <p className="content">
-        가정 양육 시에도 필요한 때에 필요한 만큼 이용할 수 있는 보육 서비스를
-        제공하여 자녀 양육에 대한 부담을 경감하고 부모의 보육 서비스 선택권을
-        보장합니다.가정 양육 시에도 필요한 때에 필요한 만큼 이용할 수 있는 보육
-        서비스를 제공하여 자녀 양육에 대한 부담을 경감하고 부모의 보육 서비스
-        선택권을 보장합니다.
-      </p>
+      <p className="howContent">{data.applmetList[0].servSeDetailNm}</p>
+      <img
+        src={data.applmetList[0].servSeDetailLink}
+        style={{ margin: "0 0 5rem 0" }}
+      />
       <p>
         <span style={{ color: "#802457" }}>아직 궁금한 것</span>이 있어요
       </p>
-      <p className="title">시간제 보육 복지정책</p>
-      <p className="content">
-        가정 양육 시에도 필요한 때에 필요한 만큼 이용할 수 있는 보육 서비스를
-        제공하여 자녀 양육에 대한 부담을 경감하고 부모의 보육 서비스 선택권을
-        보장합니다.가정 양육 시에도 필요한 때에 필요한 만큼 이용할 수 있는 보육
-        서비스를 제공하여 자녀 양육에 대한 부담을 경감하고 부모의 보육 서비스
-        선택권을 보장합니다.
-      </p>
+      {data.inqplCtadrList ? (
+        <>
+          {" "}
+          <p className="title">문의처</p>
+          {data.inqplCtadrList.map((item) => {
+            return (
+              <p className="content">
+                {item.servSeDetailNm} {item.servSeDetailLink}
+              </p>
+            );
+          })}
+        </>
+      ) : (
+        ""
+      )}
+      {data.inqplHmpgReldList ? (
+        <>
+          {" "}
+          <p className="title">사이트</p>
+          <p className="content">
+            {data.inqplHmpgReldList[0].servSeDetailNm}{" "}
+            {data.inqplHmpgReldList[0].servSeDetailLink}
+          </p>
+        </>
+      ) : (
+        ""
+      )}
+      {data.basfrmList ? (
+        <>
+          {" "}
+          <p className="title">사이트</p>
+          <p className="content">
+            {data.basfrmList[0].servSeDetailNm}{" "}
+            {data.basfrmList[0].servSeDetailLink}
+          </p>
+        </>
+      ) : (
+        ""
+      )}
+      {data.baslawList ? (
+        <>
+          {" "}
+          <p className="title">근거 법령</p>
+          {data.baslawList.map((item) => {
+            return (
+              <p className="content">
+                {item.servSeDetailNm} :{" "}
+                <a href={item.servSeDetailLink}>{item.servSeDetailLink}</a>
+              </p>
+            );
+          })}
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
