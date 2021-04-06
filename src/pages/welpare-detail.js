91b25a43bc0eb7f84c9e859524ba108703dba044
김해린 <rin3583@naver.com>
2021-04-06 10:52:39 +0900

    [EDIT] detail view server

--- src/pages/welpare-detail.js
+++ src/pages/welpare-detail.js
@@ -1,7 +1,16 @@
 import { DetailView } from "../components/Detail";
+import { CategoryDetail } from "../lib/api/category";
+import React from "react";
 
-const Detail = () => {
-  return <DetailView></DetailView>;
+const Detail = ({ data }) => {
+  return <DetailView content={data}></DetailView>;
+};
+
+Detail.getInitialProps = async (context) => {
+  const getCategoryDetail = await (await CategoryDetail(context.query.id)).data;
+  return {
+    data: getCategoryDetail,
+  };
 };
 
 export default Detail;
