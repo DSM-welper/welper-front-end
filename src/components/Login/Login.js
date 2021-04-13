63791d4b8f4c9a8414cb4a628b6bb1cea8923d28
Srim <deamoon0801@naver.com>
2021-04-13 10:31:23 +0900

    [ADD] <Login> UI

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,40 @@
-import React from "react";
+import Image from "next/image";
 import AuthPageTemplate from "../common/AuthPageTemplate/AuthPageTemplate";
+import "./Login.scss";
+import { email, lock } from "../../assets/img";
+import Link from "next/link";
 
 const Login = () => {
   return (
     <AuthPageTemplate>
-      hi
+      <div className="auth-container">
+        <h1>WELCOME TO WELPER</h1>
+        <p className="auth-container-description">지금 로그인 하여 더 나은 세상을 함께해보세요.</p>
+        <div className="auth-container-input">
+          <label>
+            Email
+            <div className="auth-input">
+              <Image src={email} width={28} height={22} />
+              <input placeholder="이메일을 입력하세요" />
+            </div>
+          </label>
+        </div>
+        <div className="auth-container-input">
+          <label>
+            Password
+            <div className="auth-input">
+              <Image src={lock} width={20} height={25} />
+              <input placeholder="비밀번호를 입력하세요" />
+            </div>
+          </label>
+          <Link href="sign-up">
+            <p className="auth-container-link">
+              아직 <strong>계정이 없으신가요?</strong>
+            </p>
+          </Link>
+        </div>
+        <button className="auth-container-button">LOGIN</button>
+      </div>
     </AuthPageTemplate>
   );
 };
