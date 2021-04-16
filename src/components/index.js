2abfcda02a334eb4ecfbee8c924bae31e19fd50b
김해린 <rin3583@naver.com>
2021-04-16 23:42:20 +0900

    [ADD] signUp index

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export { default as FirstView } from "./FirstView/FirstView";
 export { default as QnA } from "./QnA/Qna";
+export { default as SignUpEmail } from "./SignUp/SignUpEmail";
+export { default as SignUpData } from "./SignUp/SignUpData";
+export { default as SignUpAuth } from "./SignUp/SignUpAuth";
