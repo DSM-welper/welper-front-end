068188a169a35f22e80fd9f074c64d583c78fd15
김해린 <rin3583@naver.com>
2021-04-18 15:34:01 +0900

    [MODIFY] Merge

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,14 +1,8 @@
 import React from "react";
 import "./Header.scss";
-<<<<<<< HEAD
-import { welper_logo, search, user_color, triangle } from "../../../assets/img";
-
-const Header = () => {
-=======
 import { welper_logo, welper_icon, triangle } from "../../../assets/img";
 import Searchbar from "../Searchbar/Searchbar";
 const Header = ({ handleSearch }) => {
->>>>>>> develop
   return (
     <div className="header-container">
       <img src={welper_logo} className="logo" />
@@ -16,11 +10,7 @@ const Header = ({ handleSearch }) => {
       <div className="search-container">
         <Searchbar handleSearch={handleSearch} />
         <div className="header-profile">
-<<<<<<< HEAD
           <img />
-=======
-          <img src={welper_icon} />
->>>>>>> develop
         </div>
         <img src={triangle} className="menu-img" />
       </div>
