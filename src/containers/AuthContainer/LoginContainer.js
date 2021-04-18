ece4f4c66fa8ca74b044d526460bf3f926cf45dd
Srim <deamoon0801@naver.com>
2021-04-18 14:22:50 +0900

    [ADD] token cookie set

--- src/containers/AuthContainer/LoginContainer.js
+++ src/containers/AuthContainer/LoginContainer.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Login from "../../components/Login/Login";
 import { login } from "../../lib/api/user";
 import { ErrorToast, SuccessToast } from "../../lib/toast";
+import cookie from "js-cookie";
 
 const LoginContainer = () => {
   const [loginData, setLoginData] = useState({ email: "", password: "" });
@@ -11,8 +12,9 @@ const LoginContainer = () => {
   };
   const onSubmitLogin = () => {
     login(loginData)
-      .then(() => {
+      .then((res) => {
         SuccessToast("로그인 성공. 환영합니다!");
+        cookie.set("accessToken", res.accessToken, { expires: 100 });
       })
       .catch(() => {
         ErrorToast("로그인에 실패했습니다. 다시 시도하세요.");
