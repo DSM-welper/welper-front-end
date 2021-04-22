38580c573759ad52ca0c2f7c61c28e82fc18f1a3
Srim <deamoon0801@naver.com>
2021-04-22 16:59:34 +0900

    [ADD] refresh token

--- src/components/FirstView/FirstView.js
+++ src/components/FirstView/FirstView.js
@@ -2,13 +2,28 @@ import { useEffect } from "react";
 import "./FirstView.scss";
 import { FirstScreen, SecondScreen, ThirdScreen, FourthScreen } from "./";
 import { WarningToast } from "../../lib/toast";
+import cookie from "js-cookie";
+import client from "../../lib/api/client";
+import { refreshToken } from "../../lib/api/user";
+import { useRouter } from "next/router";
 
 const FirstView = ({ list }) => {
+  const router = useRouter();
   useEffect(() => {
     if (list === "INVALID_TOKEN") {
-      WarningToast("다시 로그인 해주세요.");
+      const retoken = cookie.get("refreshToken");
+      client.defaults.headers.common["refreshToken"] = retoken;
+      refreshToken()
+        .then((res) => {
+          cookie.remove("accessToken");
+          cookie.set("accessToken", res.accessToken, { expires: 1800000 });
+        })
+        .catch(() => {
+          WarningToast("오류가 발생하였습니다. 로그인 해주세요.");
+          router.push("/login");
+        });
     }
-  });
+  }, []);
   return (
     <>
       <FirstScreen />
