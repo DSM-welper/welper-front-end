3762f00ec5e2397c40c768a45937a46164abc21c
김해린 <rin3583@naver.com>
2021-05-15 00:32:42 +0900

    [MODIFY] Category List

--- src/containers/SearchContainer/CategorySearchContainer.js
+++ src/containers/SearchContainer/CategorySearchContainer.js
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { ListItem, ListDefault, CategoryHedaer } from "../../components";
 import { PageTemplate } from "../../components/common";
 import { CategoryTag } from "../../lib/api/category";
-import { WarningToast, ErrorToast } from "../../lib/toast";
+import { WarningToast } from "../../lib/toast";
+import { useInView } from "react-intersection-observer";
+
+import { category_datail } from "../../assets/img";
 
 const CategorySearchContainer = () => {
   let [getData, setGetData] = useState([]);
@@ -10,6 +13,8 @@ const CategorySearchContainer = () => {
   let [listData, setList] = useState([]);
   let [page, setPage] = useState(0);
   let [toTalPage, setTotal] = useState(0);
+  let [ref, inView] = useInView();
+
   const category = (data) => {
     setGetData(data);
     if (data.length == 0) {
@@ -22,42 +27,28 @@ const CategorySearchContainer = () => {
     if (getData.length != 0) getSearch();
   }, [getData]);
 
+  useEffect(() => {
+    if (inView) {
+      scrollPage();
+    }
+  }, [inView]);
+
   const getSearch = async () => {
-    await CategoryTag(page, getData)
-      .then((list) => {
-        setList(list.data.servList);
-        setTotal(list.data.toTalPage);
-        setIsDefault(false);
-      })
-      .catch((err) => {
-        if (err.response.data.code === "NON_EXIST_PAGE")
-          ErrorToast("검색 결과가 없습니다.");
-      });
+    const categoryList = await (await CategoryTag(page, getData)).data;
+    setList(categoryList.servList);
+    setTotal(categoryList.toTalPage);
+    if (categoryList) setIsDefault(false);
     setPage((page) => page + 1);
   };
 
   const scrollPage = async () => {
-    await CategoryTag(page, getData)
-      .then((list) => {
-        setList(listData.concat(list.data.servList));
-        setPage((page) => page + 1);
-      })
-      .catch((err) => {
-        console.log(err.response.code);
-      });
-  };
-
-  const infiniteScroll = () => {
-    const scrollHeight = document.getElementById("list").scrollHeight;
-    const scrollTop = document.getElementById("list").scrollTop;
-    const clientHeight = document.getElementById("list").clientHeight;
-    if (scrollTop + clientHeight === scrollHeight) {
-      if (toTalPage == page) {
-        WarningToast("마지막 페이지입니다.");
-        return 0;
-      } else {
-        scrollPage();
-      }
+    if (toTalPage == page) {
+      WarningToast("마지막 페이지입니다.");
+      return 0;
+    } else {
+      const list = await (await CategoryTag(page, getData)).data;
+      setList(listData.concat(list.servList));
+      setPage((page) => page + 1);
     }
   };
 
@@ -68,8 +59,8 @@ const CategorySearchContainer = () => {
         {isDefault ? (
           <ListDefault />
         ) : (
-          <div className="list-container" id="list" onScroll={infiniteScroll}>
-            <ListItem list={listData} />
+          <div className="list-container" id="list">
+            <ListItem list={listData} ref={ref} />
           </div>
         )}
       </PageTemplate>
