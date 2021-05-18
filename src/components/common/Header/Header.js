c9ea995f490669c0dd668befbadef353bac09945
김해린 <rin3583@naver.com>
2021-05-18 10:08:04 +0900

    [MODIFY] Header name, Router

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,12 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Header.scss";
 import { welper_logo, welper_icon, triangle } from "../../../assets/img";
 import Searchbar from "../Searchbar/Searchbar";
 import Router from "next/router";
 import cookie from "js-cookie";
 import { SuccessToast } from "../../../lib/toast";
+import { GetMyProfile } from "../../../lib/api/user";
 
 const Header = ({ handleSearch }) => {
+  let [name, setName] = useState("");
   const logout = () => {
     SuccessToast("이용해 주셔서 감사합니다. \n 로그아웃 되셨습니다.");
     setTimeout(() => {
@@ -16,6 +18,9 @@ const Header = ({ handleSearch }) => {
   };
   useEffect(() => {
     onMenu();
+    GetMyProfile().then((res) => {
+      setName(res.data.name);
+    });
   }, []);
 
   const onMenu = () => {
@@ -25,7 +30,11 @@ const Header = ({ handleSearch }) => {
   };
   return (
     <div className="header-container">
-      <img src={welper_logo} className="logo" />
+      <img
+        src={welper_logo}
+        className="logo"
+        onClick={() => Router.push("/")}
+      />
 
       <div className="search-container">
         <Searchbar handleSearch={handleSearch} />
@@ -35,7 +44,7 @@ const Header = ({ handleSearch }) => {
         <img src={triangle} className="menu-img" onClick={onMenu} />
         <div className="menu">
           <img src={welper_icon} id="img-menu" />
-          <h5>김해린</h5>
+          <h5>{name}</h5>
           <h5 onClick={() => Router.push("/my-profile")}>프로필 설정</h5>
           <h6 onClick={logout}>로그아웃</h6>
         </div>
