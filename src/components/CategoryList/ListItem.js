0ed25f851b719bec709d0e9c47c0f375d83e5d15
김해린 <rin3583@naver.com>
2021-05-22 17:07:10 +0900

    [MODIFY] category list

--- src/components/CategoryList/ListItem.js
+++ src/components/CategoryList/ListItem.js
@@ -1,9 +1,14 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useEffect, useState } from "react";
 import "./Category.scss";
-import { category_datail } from "../../assets/img";
+import { category_datail, bookmark, bookmark_color } from "../../assets/img";
 import Router from "next/router";
+import { BookMark } from "../../lib/api/user";
 
 const ListItem = forwardRef((props, ref) => {
+  let markSrc;
+  props.isBookMark ? (markSrc = bookmark_color) : (markSrc = bookmark);
+  let [isMark, setMark] = useState(markSrc);
+
   const getDetail = (id) => {
     Router.push({
       pathname: "/welper-detail",
@@ -11,40 +16,52 @@ const ListItem = forwardRef((props, ref) => {
     });
   };
 
+  const bookMark = (id) => {
+    if (props.isBookMark === false) {
+      BookMark(id)
+        .then((res) => {
+          if (res.status === 200)
+            alert("저장되었습니다. 마이페이지에서 확인하세요.");
+          setMark(bookmark_color);
+        })
+        .catch((err) => {
+          if (err.response.data.code == "INVALID_TOKEN")
+            WarningToast("로그인 후에 사용 가능한 기능입니다. 로그인해 주세요");
+        });
+    }
+  };
+
   return (
     <>
-      <h3 id="search"></h3>
-      {props.list.map((item, idx) => (
-        <React.Fragment key={idx}>
-          {props.list.length - 1 == idx ? (
-            <div
-              className="list-box"
-              onClick={() => getDetail(item.servId)}
-              ref={ref}
-            >
-              <p>{item.servNm}</p>
-              <div className="content-box">
-                <span>{item.servDgst.substring(0, 120)}</span>
-              </div>
-              <img
-                src={category_datail}
-                onClick={() => getDetail(item.servId)}
-              />
+      <React.Fragment>
+        {props.length - 1 == props.idx ? (
+          <div className="list-box" ref={ref}>
+            <p>{props.list.servNm}</p>
+            <div className="content-box">
+              <span>{props.list.servDgst.substring(0, 120)}</span>
             </div>
-          ) : (
-            <div className="list-box" onClick={() => getDetail(item.servId)}>
-              <p>{item.servNm}</p>
-              <div className="content-box">
-                <span>{item.servDgst.substring(0, 120)}</span>
-              </div>
-              <img
-                src={category_datail}
-                onClick={() => getDetail(item.servId)}
-              />
+            <img
+              src={category_datail}
+              onClick={() => getDetail(props.list.servId)}
+            />
+
+            <img src={isMark} onClick={() => bookMark(props.list.servId)} />
+          </div>
+        ) : (
+          <div className="list-box">
+            <p>{props.list.servNm}</p>
+            <div className="content-box">
+              <span>{props.list.servDgst.substring(0, 120)}</span>
             </div>
-          )}
-        </React.Fragment>
-      ))}
+            <img
+              src={category_datail}
+              onClick={() => getDetail(props.list.servId)}
+            />
+
+            <img src={isMark} onClick={() => bookMark(props.list.servId)} />
+          </div>
+        )}
+      </React.Fragment>
     </>
   );
 });
