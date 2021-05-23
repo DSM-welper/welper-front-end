dc1d355f0288a9c69eb047d119fd1ee541c7ead8
Srim <deamoon0801@naver.com>
2021-05-23 15:00:04 +0900

    [FIX] Can't resolve './QnA/Qna'

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as FirstView } from "./FirstView/FirstView";
-export { default as QnA } from "./QnA/Qna";
+export { default as QnA } from "./QnA/QnA";
 export { default as SignUpEmail } from "./SignUp/SignUpEmail";
 export { default as SignUpData } from "./SignUp/SignUpData";
 export { default as SignUpAuth } from "./SignUp/SignUpAuth";
