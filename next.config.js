2f526a26c1e9eb3c90b4347bb6d251e562731ee7
Srim <deamoon0801@naver.com>
2021-05-23 22:52:31 +0900

    [FIX] next export images error

--- next.config.js
+++ next.config.js
@@ -4,8 +4,10 @@ const withSass = require("@zeit/next-sass");
 module.exports = withCSS(
   withSass({
     distDir: "build",
-    exportTrailingSlash: true,
-
+    trailingSlash: true,
+    images: {
+      loader: 'imgix',
+    },
     webpack(config, options) {
       config.module.rules.push({
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
