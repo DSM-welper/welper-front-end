fea2be4af4d5663f51a6e2f23067b9d6b9d71bb2
Srim <deamoon0801@naver.com>
2021-05-23 22:34:43 +0900

    [UPDATE] <next.config.js> distDir

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withSass = require("@zeit/next-sass");
 
 module.exports = withCSS(
   withSass({
+    distDir: "build",
+    exportTrailingSlash: true,
+
     webpack(config, options) {
       config.module.rules.push({
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
