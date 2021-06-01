48ea13445630d8b8dc51d761dabba1bdd17f8b12
Srim <deamoon0801@naver.com>
2021-06-01 20:12:57 +0900

    [DELETE] <MyPage> nickname text

--- src/components/Profile/MyPage.js
+++ src/components/Profile/MyPage.js
@@ -15,14 +15,10 @@ const MyPage = ({ data }) => {
       <PageTemplate>
         <div className="profile-container">
           <div className="profile-header">마이페이지</div>
-          <h4>해린 님의 북마크 목록입니다.</h4>
+          <h4>북마크 목록입니다.</h4>
           <div className="my-list-box">
             {data.bookMark.map((item, key) => (
-              <div
-                className="list"
-                key={key}
-                onClick={() => detailView(item.servId)}
-              >
+              <div className="list" key={key} onClick={() => detailView(item.servId)}>
                 <span>{item.servNm}</span>
                 <p>{item.servDgst.substring(0, 80)}</p>
               </div>
